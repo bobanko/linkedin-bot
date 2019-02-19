17aa74eca86a8b600432a4503ccc48b23f465b79
bobanko <mishko.vladimir@gmail.com>
2019-02-19 20:03:56 +0200

    creds sample

---
+++ sample.credentials.js
@@ -0,0 +1,12 @@
+/**TODO:
+ * - set your username/password here
+ * - rename this file to `credentials.js`
+ * ⚠️ DO NOT COMMIT THIS FILE
+ */
+
+const credentials = {
+  username: "",
+  password: ""
+};
+
+module.exports = credentials;
